7a0b244d9058d03d829aa2f0c40231167dcbea09
nekoTheShadow <h.nakamura0903@gmail.com>
2023-12-09 12:00:00 +0900

    Day 09 : Part 01

---
+++ day09_01/main.rb
@@ -0,0 +1,17 @@
+sum = 0
+
+IO.foreach('./input.txt', :chomp => true) do |line|
+  numbers = line.split.map(&:to_i)
+  level = 0
+  place_holder = 0
+  while numbers.uniq.size > 1
+    place_holder += numbers[-1]
+    level += 1
+    numbers = (0...numbers.size-1).map{|i| numbers[i+1]-numbers[i]}
+    
+  end
+  place_holder += numbers[-1]
+  sum += place_holder
+end
+
+p sum
